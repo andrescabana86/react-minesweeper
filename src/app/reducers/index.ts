3e92f6b7893ccb46d014e5e37f54711d5d751ff7
andrescabana86 <andrescabana86@gmail.com>
2018-06-24 22:38:06 -0300

    feat: changing mock squema

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,29 +1,33 @@
 import {
   START_NEW_GAME,
   REVEAL_CELL } from '../actions/types';
-import { revealCell } from '../helpers';
+import { revealCellAndTestPosition } from '../helpers';
 
 const initialState:any = {
   gameTitle: 'Welcome to Minesweeper!',
   gameStarted: true,
+  cellsByXY: {
+    '1,1': { position: '1,1', status: 'hidden', type: 'space', value: 0 },
+    '1,2': { position: '1,2', status: 'revealed', type: 'number', value: 1 },
+    '1,3': { position: '1,3', status: 'revealed', type: 'number', value: 2 },
+    '1,4': { position: '1,4', status: 'revealed', type: 'number', value: 3 },
+    '1,5': { position: '1,5', status: 'revealed', type: 'bomb', value: 0 },
+    '1,6': { position: '1,6', status: 'revealed', type: 'flag', value: 0 },
+    '2,1': { position: '2,1', status: 'hidden', type: 'space', value: 0 },
+    '2,2': { position: '2,2', status: 'hidden', type: 'space', value: 0 },
+    '2,3': { position: '2,3', status: 'hidden', type: 'space', value: 0 },
+    '2,4': { position: '2,4', status: 'hidden', type: 'space', value: 0 },
+    '2,5': { position: '2,5', status: 'hidden', type: 'space', value: 0 },
+    '2,6': { position: '2,6', status: 'hidden', type: 'space', value: 0 },
+  },
   board: [
     {
       position: '1', children: [
-        { position: '1,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '1,2', status: 'revealed', type: 'number', value: 1 },
-        { position: '1,2', status: 'revealed', type: 'number', value: 2 },
-        { position: '1,2', status: 'revealed', type: 'number', value: 3 },
-        { position: '1,3', status: 'revealed', type: 'bomb', value: 0 },
-        { position: '1,4', status: 'revealed', type: 'flag', value: 0 },
+        '1,1', '1,2', '1,3', '1,4', '1,5', '1,6',
       ],
     }, {
       position: '2', children: [
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
-        { position: '2,1', status: 'hidden', type: 'space', value: 0 },
+        '2,1', '2,2', '2,3', '2,4', '2,5', '2,6',
       ],
     },
   ],
@@ -41,7 +45,7 @@ export default (state = initialState, action: any) => {
       };
 
     case REVEAL_CELL:
-      revealCell(state, action.cellPosition);
+      revealCellAndTestPosition(state, action.cellPosition);
       return {
         ...state,
       };
