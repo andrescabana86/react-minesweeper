e24f3598c54d79a9b0864209066d14dcc2d619ac
andrescabana86 <andrescabana86@gmail.com>
2018-06-24 22:30:57 -0300

    feat: reveal cell on click

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -1,3 +1,6 @@
+import { CELL_STATUSES } from '../constants';
+
 export const revealCell = (state:any, cellPosition:string) => {
-  console.log(cellPosition);
+  const cell = state.board[0].children[0];
+  cell.status = CELL_STATUSES.revealed;
 };
