ce5be09d23fcdeb1e4065accdf6efe352f22deb6
andrescabana86 <andrescabana86@gmail.com>
2018-06-24 22:26:46 -0300

    feat: add reveal cell  action

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,3 +1,6 @@
-import { START_NEW_GAME } from './types';
+import {
+  START_NEW_GAME,
+  REVEAL_CELL } from './types';
 
 export const startNewGame = () => ({ type: START_NEW_GAME });
+export const revealCell = (position:string) => ({ type: REVEAL_CELL, cellPosition: position });
--- src/app/actions/types/index.ts
+++ src/app/actions/types/index.ts
@@ -1,1 +1,2 @@
 export const START_NEW_GAME = 'START_NEW_GAME';
+export const REVEAL_CELL = 'REVEAL_CELL';
