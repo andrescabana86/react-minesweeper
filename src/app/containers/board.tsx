03ee3f191641894474f3ebf910a9507cdae03f20
andrescabana86 <andrescabana86@gmail.com>
2018-06-24 21:08:23 -0300

    feat: add board component

---
+++ src/app/containers/board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './board.css';
+
+export default class Board extends React.Component {
+  render() {
+    return (
+      <section className="board__container">
+
+      </section>
+    );
+  }
+}
