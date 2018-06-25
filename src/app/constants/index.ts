21bb0720a68deecc41592daf448b2e423d43e1eb
andrescabana86 <andrescabana86@gmail.com>
2018-06-24 22:07:36 -0300

    feat: add cell constants

---
+++ src/app/constants/index.ts
@@ -0,0 +1,12 @@
+export const CELL_STATUSES = Object.freeze({
+  revealed: 'revealed',
+  hidden: 'hidden',
+});
+
+export const CELL_TYPES = Object.freeze({
+  bomb: 'bomb',
+  flag: 'flag',
+  number: 'number',
+  space: 'space',
+  unknown: 'unknown',
+});
